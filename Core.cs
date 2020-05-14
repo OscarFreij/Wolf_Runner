7d6bcb4f76edeb96c13921904666ee3c2987a148
Oscar Freij <otg020313@gmail.com>
2020-05-14 17:14:46 +0200

    Basic Code Structure in place

---
+++ Core.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Wolf_Runner
+{
+    class Core
+    {
+        public class Enviroment
+        {
+
+        }
+        
+        public class Obstacle
+        {
+
+        }
+
+        public class Player
+        {
+
+        }
+        
+        public class UI
+        {
+
+        }
+    }
+}
