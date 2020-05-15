8a356879e4a534c056ef190b43b853b5c71a3dcb
Oscar Freij <otg020313@gmail.com>
2020-05-15 21:18:45 +0200

    Added Basic Collision Logic and Movement

--- Core.cs
+++ Core.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 
@@ -118,7 +119,7 @@ namespace Wolf_Runner
             public Game1 Game { get; private set; }
             public List<Texture2D> Sprites { get; private set; }
             public Vector2 Position { get; private set; }
-            public Vector2 AccelerationForce { get; private set; }
+            public float JumpForce { get; private set; }
             public float CurrentSpeed { get; private set; }
             public float Weight { get; private set; }
             public int MaxHealth { get; private set; }
@@ -128,11 +129,22 @@ namespace Wolf_Runner
             public bool isGrounded { get; private set; }
             public Rectangle Collider { get; private set; }
             public Texture2D ColliderTexture { get; private set; }
+            public Rectangle GDCollider { get; private set; }
+            public Texture2D GDColliderTexture { get; private set; }
 
-            public Player(Game1 Game, float Weight, Vector2 AccelerationForce, int MaxHealth)
+
+            public float YAxis { get; private set; }
+            public float CurrenForce { get; private set; }
+
+            public Player(Game1 Game, float Weight, float JumpForce, int MaxHealth)
             {
                 this.Game = Game;
                 this.Position = new Vector2((this.Game.Window.ClientBounds.Width / 3) - 150, this.Game.Window.ClientBounds.Height / 2);
+
+                this.Weight = Weight;
+                this.JumpForce = JumpForce;
+                this.MaxHealth = MaxHealth;
+                this.CurrentHealth = this.MaxHealth;
 
                 AnimationTime = 0.5f;
                 CurrentAnimationTime = 0;
@@ -145,19 +157,74 @@ namespace Wolf_Runner
                 Sprites.Add(this.Game.Content.Load<Texture2D>("Sprites/Wolf/Wolf_2"));
                 Sprites.Add(this.Game.Content.Load<Texture2D>("Sprites/Wolf/Wolf_3"));
 
-                this.Collider = new Rectangle((int)(this.Position.X+3), (int)(this.Position.Y+32), 120, 62);
+                this.Collider = new Rectangle((int)(this.Position.X+3), (int)(this.Position.Y+32), 120, 58);
 
                 this.ColliderTexture = new Texture2D(Game.GraphicsDevice, 120, 32);
                 Color[] data = new Color[120 * 32];
                 for (int i = 0; i < data.Length; ++i) data[i] = new Color(0, 50, 250, 230);
-                ColliderTexture.SetData(data);
+                this.ColliderTexture.SetData(data);
+
+
+                this.GDCollider = new Rectangle((int)(this.Position.X + 3), (int)(this.Position.Y + 32 + 58), 120, 5);
+
+                this.GDColliderTexture = new Texture2D(Game.GraphicsDevice, 120, 5);
+                Color[] dataG = new Color[120 * 5];
+                for (int i = 0; i < dataG.Length; ++i) dataG[i] = new Color(230, 50, 0, 230);
+                this.GDColliderTexture.SetData(dataG);
+
+                this.CurrenForce = 0;
             }
 
             public void Tick(GameTime gameTime)
             {
+                if ((Keyboard.GetState().IsKeyDown(Keys.S) || Keyboard.GetState().IsKeyDown(Keys.Down)) && !(Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.Up)))
+                {
+                    this.YAxis = 1;
+                }
+                else if (!(Keyboard.GetState().IsKeyDown(Keys.S) || Keyboard.GetState().IsKeyDown(Keys.Down)) && (Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.Up)))
+                {
+                    this.YAxis = -1;                
+                }
+                else
+                {
+                    this.YAxis = 0;
+                }
+
+                
+
+                if (!this.GDCollider.Intersects(this.Game.Enviroment.FloorCollider) && !this.Collider.Intersects(this.Game.Enviroment.FloorCollider))
+                {
+                    this.CurrenForce += this.Game.Enviroment.GravityConstant * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                    if (YAxis > 0)
+                    {
+                        this.CurrenForce += this.YAxis * this.JumpForce;
+                    }
+                }
+                else
+                {
+                    this.CurrenForce = 0;
+                    if (YAxis < 0)
+                    {
+                        this.CurrenForce += this.YAxis * this.JumpForce;
+                    }
+                }
 
 
+                
 
+
+                if (this.Collider.Intersects(this.Game.Enviroment.FloorCollider))
+                {
+                    Position = new Vector2(Position.X, this.Game.Enviroment.Floor - 32 - 58);
+                }
+                else
+                {
+                    Position = new Vector2(Position.X, Position.Y + this.CurrenForce);
+                }
+
+                this.Collider = new Rectangle((int)(this.Position.X + 3), (int)(this.Position.Y + 32), 120, 58);
+                this.GDCollider = new Rectangle((int)(this.Position.X + 3), (int)(this.Position.Y + 32 + 58), 120, 5);
             }
 
             public void Draw(GameTime gameTime, bool DrawDebug)
@@ -171,40 +238,27 @@ namespace Wolf_Runner
                         CurrentAnimationTime = 0.0f;
                     }
 
+                    if (DrawDebug)
+                    {
+                        this.Game.spriteBatch.Draw(this.ColliderTexture, this.Collider, Color.White);
+                        this.Game.spriteBatch.Draw(this.GDColliderTexture, this.GDCollider, Color.White);
+                    }
+
+
                     if (CurrentAnimationTime <= AnimationTime * 0.25)
                     {
-                        if (DrawDebug)
-                        {
-                            this.Game.spriteBatch.Draw(this.ColliderTexture, this.Collider, Color.White);
-                        }
-
                         this.Game.spriteBatch.Draw(Sprites[0], this.Position, Color.White);
                     }
                     else if (CurrentAnimationTime <= AnimationTime * 0.50)
                     {
-                        if (DrawDebug)
-                        {
-                            this.Game.spriteBatch.Draw(this.ColliderTexture, this.Collider, Color.White);
-                        }
-
                         this.Game.spriteBatch.Draw(Sprites[1], this.Position, Color.White);
                     }
                     else if (CurrentAnimationTime <= AnimationTime * 0.75)
                     {
-                        if (DrawDebug)
-                        {
-                            this.Game.spriteBatch.Draw(this.ColliderTexture, this.Collider, Color.White); ;
-                        }
-
                         this.Game.spriteBatch.Draw(Sprites[2], this.Position, Color.White);
                     }
                     else if (CurrentAnimationTime <= AnimationTime * 1)
                     {
-                        if (DrawDebug)
-                        {
-                            this.Game.spriteBatch.Draw(this.ColliderTexture, this.Collider, Color.White);
-                        }
-
                         this.Game.spriteBatch.Draw(Sprites[3], this.Position, Color.White);
                     }
                     else
